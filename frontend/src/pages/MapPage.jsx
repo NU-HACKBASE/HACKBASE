751ed95cc84c0dd72527d70168b6efccf20ff3ee
sakamoti55 <kohseimeimei@gmail.com>
2026-07-05 12:48:18 +0900

    範囲変更

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -7,7 +7,7 @@ import { useCurrentUser } from '../hooks/useCurrentUser'
 import { fetchEvents } from '../lib/eventApi'
 
 const DEFAULT_CENTER = { latitude: 35.681236, longitude: 139.767125 }
-const NEARBY_EVENT_RADIUS_METERS = 2500
+const NEARBY_EVENT_RADIUS_METERS = 5000
 
 const INITIAL_LOCATION = {
   latitude: null,
